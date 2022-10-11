4bcd20154be800302c8f474d0db23cdd054e1166
Eduardo Matias <eduardo.matias@gigalink.net.br>
2022-10-11 10:14:41 -0300

    Alteração da versão

--- lib/brcobranca/retorno/cnab400/base.rb
+++ lib/brcobranca/retorno/cnab400/base.rb
@@ -11,6 +11,8 @@ module Brcobranca
           return nil if file.blank?
 
           codigo_banco = codigo_banco_do_arquivo(file)
+          
+          puts codigo_banco
 
           case codigo_banco
           when '001'
--- lib/brcobranca/version.rb
+++ lib/brcobranca/version.rb
@@ -2,5 +2,5 @@
 #
 
 module Brcobranca
-  VERSION = '9.2.10'
+  VERSION = '9.2.11'
 end
